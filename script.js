d572e801a81dc3e47cc10295a61a020f90e2df2d
Janaina Leal <73653429+JanainaLeal@users.noreply.github.com>
2022-07-08 15:33:55 +0100

    Calculadora de Horas
    calculadora de horas de trabalho.
    Workshop REPROGRAMA

---
+++ script.js
@@ -0,0 +1,8 @@
+let entrada = document.querySelector('input')
+let saida = document.querySelector('#respostaAno')
+
+function cliquei (){
+    let ano = entrada.value
+    saida.textContent = "Resposta:" + ano
+}
+
